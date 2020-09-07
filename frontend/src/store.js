f16af9cc354d004eed274e41a8b1c72ba179a3e8
Daniel Ramirez <danielfr1987@gmail.com>
2020-09-07 13:55:12 -0600

    Devtool removed because is generating errors when not have redux extencion in or browser

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,27 +6,17 @@ const initialState = {};
 
 const middleware = [thunk];
 
-// Redux extension
-let devTools = null;
-
 // Enable state change logging only for dev environment
 if (process.env.NODE_ENV === "development") {
   console.log("NODE_ENV =>", process.env.NODE_ENV);
   const { logger } = require("redux-logger");
   middleware.push(logger);
-
-  devTools =
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    window.__REDUX_DEVTOOLS_EXTENSION__();
 }
 
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    devTools
-  )
+  compose(applyMiddleware(...middleware))
 );
 
 export default store;
